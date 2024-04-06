765afce24d006cd28316f6ba28bd4ef66a5eb7d9
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-04-06 19:06:01 +0500

    234
    423

--- harflar_o`yini (copy)/main.js
+++ harflar_o`yini (copy)/main.js
@@ -1,15 +1,8 @@
 var a
 var qiymat=["Й","Ц","У","К","Е","Н","Г","Ш","Щ","З","Х","Ъ","Ф","Ы","В","А","П","Р","О","Л","Д","Ж","Э","Я","Ч","С","М","И","Т","Ь","Б","Ю"]
-var randomIndex1
+var randomIndex1 = Math.floor(Math.random() * 36);
 var time=-1
-var stop=false
-var count_text=0
-var my_select=0
 function taxmin() { 
-    count_text=0
-    stop=false
-     my_select=0
-     randomIndex1 = Math.floor(Math.random() * 36);
     document.querySelector('.main_action').style="display:none;"
     document.querySelector('.main_game').style="display:block;"
     document.querySelector(".body_game").innerHTML=""
@@ -20,19 +13,6 @@ function taxmin() {
      var taxminiyXarif = qiymat[randomIndex]
     a.push(taxminiyXarif)    
   }
-for (let i = 0; i < 13; i++) {
-    var randomIndex = Math.floor(Math.random() * qiymat.length);
-    a[randomIndex]=a[randomIndex1]
-}
-
-for (let i = 0; i < a.length; i++) {
-if(a[i]==a[randomIndex1]){
-count_text++
-} 
-}
-
-console.log(a);
-
 for (let i = 0; i < a.length; i++) {
     document.querySelector(".body_game").innerHTML+=`<div onclick="selectP('${a[i]}',${i})" class="bukva">${a[i]}</div>`
 }
@@ -40,17 +20,16 @@ document.querySelector('.title').innerHTML=`НАЙДИ БУКВУ: ${a[randomInd
 
 }
 
-
 setInterval(() => {
-    if(time>0 && !stop){
-        time=(time-0.1).toFixed(1)
+    if(time>0){
+        time=time-1
         document.querySelector('.timer').innerHTML=`<i class='bx bx-time-five'></i> ТАЙМЕР: ${time} секунд`
     }else{
-    if(time!=-1 && !stop){
+    if(time!=-1){
         selectP('',-1)
     }
     }
-}, 100);
+}, 1000);
 
 
 
@@ -61,18 +40,13 @@ function selectP(b,key){
     console.log(a[randomIndex1],b);
     console.log(key);
 if(a[randomIndex1]==b ){
-  if(!document.querySelectorAll(".bukva")[key].disabled){
- my_select++
-   }
-    document.querySelectorAll(".bukva")[key].disabled=true
-    document.querySelectorAll(".bukva")[key].style="color:green;"
-if(my_select==count_text){
-    stop=true
-    document.querySelector('.rekord').innerHTML=`ТАЙМЕР: ${time} секунд`
-}
+    document.querySelectorAll(".bukva")[key].style="color:green !important;"
+setTimeout(() => {
+taxmin()  
+}, 1000);
 }else{
     if(key!=-1){
-        document.querySelectorAll(".bukva")[key].style="color:red;"
+        document.querySelectorAll(".bukva")[key].style="color:red !important;"
         setTimeout(() => {
             document.querySelectorAll(".bukva")[key].style="color:black !important;"
 
